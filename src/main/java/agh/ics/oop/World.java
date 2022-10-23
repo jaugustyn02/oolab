b85b512ce75b98e143c90fe19e2ef22cbe0a3e96
Jan Augustyn <jan.augusty02@gmail.com>
2022-10-23 14:31:28 +0200

    lab3_final

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,38 +1,46 @@
 package agh.ics.oop;
 
-//import java.lang.reflect.AnnotatedArrayType;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+//import java.lang.reflect.AnnotatedArrayType;
+//import java.util.List;
+//import java.util.stream.Collectors;
 
 
 public class World {
     public static void main(String[] args) {
 //        lab3
-        Animal newAnimal = new Animal();
+        Animal animal1 = new Animal();
         System.out.println(Arrays.toString(OptionsParser.parse(args)));
         for (MoveDirection move: OptionsParser.parse(args)){
-            newAnimal.move(move);
+            animal1.move(move);
         }
-        System.out.println(newAnimal);
+        System.out.println(animal1);
+
+        Animal animal2 = new Animal();
+        System.out.println(Arrays.toString(OptionsParser.parse(args)));
+        for (MoveDirection move: OptionsParser.parse(args)){
+            animal2.move(move);
+        }
+        System.out.println(animal2);
 //        lab3 end
 
 //        lab2
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
+//        Vector2d position1 = new Vector2d(1,2);
+//        System.out.println(position1);
+//        Vector2d position2 = new Vector2d(-2,1);
+//        System.out.println(position2);
+//        System.out.println(position1.add(position2));
 //        lab2 end
-
+//
 //        lab1
-        System.out.println("system wystartował");
-        List<Direction> directions = convert(args);
-        run(directions.toArray(new Direction[0]));
-        System.out.println("system zakończył działanie");
+//        System.out.println("system wystartował");
+//        List<Direction> directions = convert(args);
+//        run(directions.toArray(new Direction[0]));
+//        System.out.println("system zakończył działanie");
 //        lab1 end
     }
 
+    /*
     static List<Direction> convert(String[] args) {
         return Arrays.stream(args)
                 .filter(Arrays.asList("f", "b", "r", "l")::contains)
@@ -59,4 +67,5 @@ public class World {
         System.out.println(Arrays.stream(directions).map(Enum::name).collect(Collectors.joining(", ")));
         System.out.println("Stop");
     }
+     */
 }
